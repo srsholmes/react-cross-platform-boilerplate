7dcda1713d2c91366b2584c101c40b1bb3e4c7a5
srsholmes <srsholmes@gmail.com>
2016-07-26 19:44:02 +0100

    clean

--- node_tasks/watch-native.js
+++ node_tasks/watch-native.js
@@ -1,26 +1,15 @@
-// const nativeWatch = () =>
-//   console.log('watching native...')
-//
-// nativeWatch()
-
 const watch = require('watch')
 const fs = require('fs')
+const stripDirectory = (f) => f.split('/').slice(2).join('/')
 
 watch.createMonitor('./app/js', function (monitor) {
   monitor.on('created', function (f, stat) {
-    // Handle new files
-    console.log('file created')
+    fs.createReadStream(f).pipe(fs.createWriteStream(`./ApplicationNative/ApplicationNative/app/${stripDirectory(f)}`));
   })
   monitor.on('changed', function (f, curr, prev) {
-    const outputFile = f.split('/').slice(2).join('/')
-    fs.createReadStream(f).pipe(fs.createWriteStream(`./ApplicationNative/ApplicationNative/app/${outputFile}`));
+    fs.createReadStream(f).pipe(fs.createWriteStream(`./ApplicationNative/ApplicationNative/app/${stripDirectory(f)}`));
   })
   monitor.on('removed', function (f, stat) {
     // Handle removed files
   })
 })
-
-console.log('***** WATCHER ****')
-
-
-// Set up watch here. Detect which file has been chnaged and copy it oveer to the directory in application native.
